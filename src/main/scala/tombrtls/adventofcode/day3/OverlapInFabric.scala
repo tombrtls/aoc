04d0f2f32f56f7cf4fe26cd0ef4acd2906876589
tombrtls <tombrtls@gmail.com>
2018-12-06 21:54:30 +0100

    Implemented day3 assignement 1

--- src/main/scala/tombrtls/adventofcode/day3/OverlapInFabric.scala
+++ src/main/scala/tombrtls/adventofcode/day3/OverlapInFabric.scala
@@ -2,23 +2,39 @@ package tombrtls.adventofcode.day3
 
 import tombrtls.adventofcode.Assignment
 
+case class Coordinates(x: Int, y: Int)
+case class Square(id: Int, x: Int, y: Int, width: Int, height: Int) {
+  val horizontalRange = x until (x + width)
+  val verticalRange = y until (y + height)
+}
+
 object OverlapInFabric extends Assignment[Seq[Square], Int] {
   def main(args: Array[String]): Unit = start
 
   override val sampleInputFile: String = "/day3/sample.txt"
-  override val sampleExpectation: Int = 2
+  override val sampleExpectation: Int = 4
   override val inputFile: String = "/day3/input.txt"
 
-  private val squarePattern = "#(\\d) @ (\\d),(\\d): (\\d)x(\\d)".r
+  private val squarePattern = "#(\\d*) @ (\\d*),(\\d*): (\\d*)x(\\d*)".r
   override def processLines(lines: Seq[String]): Seq[Square] =
     lines.map { line =>
       line match {
-        case squarePattern(id, startIndex, endIndex, width, height) =>
-          Square(id.toInt, startIndex.toInt, endIndex.toInt, width.toInt, height.toInt)
+        case squarePattern(id, x, y, width, height) =>
+          Square(id.toInt, x.toInt, y.toInt, width.toInt, height.toInt)
       }
     }
 
-  override def implementation(input: Seq[Square]): Int = {
-    0
+  override def implementation(input: Seq[Square]): Int = tilesOverlap(input)
+
+  def tilesOverlap(squares: Seq[Square]): Int =
+    squares.flatMap(squaresToCoordinates)
+      .groupBy { coordinates => coordinates }
+      .count { case (_, items) => items.length > 1 }
+
+  def squaresToCoordinates(square: Square): Seq[Coordinates] = {
+    for (
+      x <- square.horizontalRange;
+      y <- square.verticalRange
+    ) yield Coordinates(x, y)
   }
 }
--- src/main/scala/tombrtls/adventofcode/day3/Square.scala
+++
@@ -1,3 +0,0 @@
-package tombrtls.adventofcode.day3
-
-case class Square(id: Int, start: Int, end: Int, width: Int, height: Int)
