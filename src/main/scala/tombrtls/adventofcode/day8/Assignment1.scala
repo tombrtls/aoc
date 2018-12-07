f165a5c230c7d354d2ea9d33bd5f1a25b33b7319
tombrtls <tombrtls@gmail.com>
2018-12-07 02:40:50 +0100

    Prepared for the upcoming days

---
+++ src/main/scala/tombrtls/adventofcode/day8/Assignment1.scala
@@ -0,0 +1,18 @@
+package tombrtls.adventofcode.day8
+
+import tombrtls.adventofcode.Assignment
+
+object SampleAssignment extends Assignment[Seq[String], Int] {
+  def main(args: Array[String]): Unit = startAssignment
+
+  override val day: Int = 8
+  override val testCases: Seq[(String, Int)] = Seq(
+    ("sample.txt", 0)
+  )
+
+  override val inputFileName: String = "input.txt"
+
+  override def processLines(lines: Seq[String]): Seq[String] = ???
+
+  override def implementation(input: Seq[String]): Int = ???
+}
