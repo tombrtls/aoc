4f2963a11c599d156745e78ebf46f1f7ad9ecca8
tombrtls <tombrtls@gmail.com>
2018-12-07 02:10:35 +0100

    Extended the sample

--- src/main/scala/tombrtls/adventofcode/sample/SampleAssignment.scala
+++ src/main/scala/tombrtls/adventofcode/sample/SampleAssignment.scala
@@ -4,7 +4,10 @@ import tombrtls.adventofcode.Assignment
 
 object SampleAssignment extends Assignment[Seq[String], Int] {
   override val day: Int = 1
-  override val testCases: Seq[(String, Int)] = Seq()
+  override val testCases: Seq[(String, Int)] = Seq(
+    ("sample_1.txt", 0),
+    ("sample_2.txt", 0)
+  )
   override val inputFileName: String = "input.txt"
 
   override def processLines(lines: Seq[String]): Seq[String] = ???
